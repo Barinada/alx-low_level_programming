dfc529789a09c22fefff1b3b37967fe3064c5d60
Barinada <vicnadelo4@gmail.com>
2023-07-12 19:34:53 -0700

    update

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,29 +1,10 @@
 #include "main.h"
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-unsigned int _strspn(char *s, char *accept)
+int _isalpha(int c)
 {
-	unsigned int n = 0;
-	int r;
-
-
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-			return (n);
-		}
-		s++;
-	}
-	return (n);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
